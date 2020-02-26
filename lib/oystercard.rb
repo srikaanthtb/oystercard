d51af4935cf037ebb586b5a855a7566cb3b68ab7
Student <student@Makerss-MacBook-Air.local>
2020-02-26 17:44:32 +0000

    step 12 completed looking at the solution like cowards

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,18 @@
 class Oystercard
-  attr_reader :balance, :max_balance, :limit, :status, :min_balance, :min_charge, :entry_station
+  attr_reader :balance, :max_balance, :limit, :status, :min_balance, :min_charge, :entry_station, :exit_station,
+   :journey
+
   MAX_BALANCE = 90
   MIN_BALANCE = 1
   MINIMUM_CHARGE = 5
   def initialize
+    @journey = []
     @balance = 0
     @max_balance = MAX_BALANCE
     @min_balance = MIN_BALANCE
     @min_charge =  MINIMUM_CHARGE
     @entry_station = nil
+    @exit_station = nil
   end
   def top_up(money)
     @balance += money
@@ -18,18 +22,24 @@ class Oystercard
 
 
 
-def touch_in(station)
+def touch_in(entry_station)
   raise "not enough money to make journey" if @balance < @min_balance
-  @entry_station = station
+  @entry_station = entry_station
+  @journey.push(entry_station)
+  
 end
 
 def in_journey?
   !!entry_station
 end
 
-def touch_out
+def touch_out(exit_station)
+  @exit_station = exit_station
   deduct(@min_charge)
+  @journey.push({entry_station:entry_station, exit_station:exit_station})
   @entry_station = nil
+  
+  exit_station
 end
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
-let(:station) { double :station}
+let(:entry_station) { double :station}
+let(:exit_station) { double :station}
+let(:journey){ {entry_station: entry_station, exit_station: exit_station} }
+
+
 
   it "checks a default balance" do
     expect(subject).to have_attributes(balance: 0)
@@ -25,38 +29,66 @@ let(:station) { double :station}
 
   it "charges money when journey starts" do
     subject.top_up(50)
-    subject.touch_in(station)
-expect{ subject.touch_out }.to change{ subject.balance }.by(-Oystercard::MINIMUM_CHARGE)
+    subject.touch_in(entry_station)
+expect{ subject.touch_out(exit_station) }.to change{ subject.balance }.by(-Oystercard::MINIMUM_CHARGE)
   end
 
 it "putsd a entry station on touch in" do
   subject.top_up(50)
-  subject.touch_in(station)
-  expect( subject.entry_station).to eq station
+  subject.touch_in(entry_station)
+  expect(subject.entry_station).to eq entry_station
 end
+
+
 
 describe "#in_journey?" do
   it "check if card is in transit" do
     subject.top_up(50)
-    subject.touch_in(station)
+    subject.touch_in(entry_station)
     expect(subject.in_journey?).to eq true
   end
 
   it "makes error if there isnt enough money to make journey" do
-    expect{subject.touch_in(station)}.to raise_error "not enough money to make journey"
+    expect{subject.touch_in(entry_station)}.to raise_error "not enough money to make journey"
   end
 
   it "check if card is not in transit" do
-    subject.touch_out
+    subject.touch_out(exit_station)
     expect(subject.in_journey?).to eq false
   end
 it "check that the station is forgotten on exit" do
   subject.top_up(50)
-  subject.touch_in(station)
-  subject.touch_out
+  subject.touch_in(entry_station)
+  subject.touch_out(exit_station)
   expect( subject.entry_station).to eq nil
 end
+it "touch out remembers exit station" do
+  subject.touch_out(exit_station)
+  expect(subject.exit_station).to eq exit_station
 
 end
 
+it 'stores exit station' do 
+  subject.top_up(50)
+  subject.touch_in(entry_station)
+  subject.touch_out(exit_station)
+  expect(subject.exit_station).to eq exit_station
 end
+
+it 'stores a journey' do 
+  subject.top_up(50)
+  subject.touch_in(entry_station)
+  subject.touch_out(exit_station)
+  expect(subject.journey).to include journey
+end
+
+# In this test we are testing the array with JOURNEYS.
+
+it 'has an empty list of journeys by default' do 
+  expect(subject.journey).to be_empty
+end
+
+end
+end
+
+
